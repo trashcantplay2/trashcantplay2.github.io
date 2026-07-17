b9e5304a0da4d1fe071e25dbcc5ecfbe436a8c8c
unknown <trashcantplay2@gmail.com>
2026-07-17 14:44:50 -0500

    Updaet

--- myforfeit/Program.cs
+++ myforfeit/Program.cs
@@ -9,6 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped<TelegramService>();
+// builder.Services.AddScoped<TelegramService>(); // Removed TelegramService registration
 
 await builder.Build().RunAsync();
