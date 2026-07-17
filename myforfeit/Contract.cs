7cd19393312a8d391e5afb96c62c8a73714c4afb
unknown <trashcantplay2@gmail.com>
2026-07-16 19:07:26 -0500

    first commit

---
+++ myforfeit/Contract.cs
@@ -0,0 +1,11 @@
+﻿namespace myforfeit;
+
+public class Contract
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public DateTime Deadline { get; set; } = DateTime.Now.AddDays(1);
+    public bool IsCompleted { get; set; }
+    public bool NotificationSent { get; set; }
+    public int Stakes { get; set; } = 5; // Default to $5
+}
