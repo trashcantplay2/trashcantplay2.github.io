dc7b85c69ef8d498fccf26165ba07d5c6645f037
unknown <trashcantplay2@gmail.com>
2026-07-17 16:02:10 -0500

    Update

--- myforfeit/obj/Debug/net8.0/myforfeit.AssemblyInfo.cs
+++ myforfeit/obj/Debug/net8.0/myforfeit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myforfeit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e5304a0da4d1fe071e25dbcc5ecfbe436a8c8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac41b7c70ad3fafd1faf5a833215f131dcc7247")]
 [assembly: System.Reflection.AssemblyProductAttribute("myforfeit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myforfeit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
